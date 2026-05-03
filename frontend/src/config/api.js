afb3b51adb37630f0ca8a0235b6273b0860aa5ba
mahmoudalimohameed4 <mahmoudalimohameed4@gmail.com>
2026-05-03 22:16:47 +0300

    Debug: Add log for API URL

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,6 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL || window.location.origin;
 
+// Debugging (Remove in final production)
+console.log('Current API URL:', API_BASE_URL);
+
 export default API_BASE_URL;
